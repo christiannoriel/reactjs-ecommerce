0cdfcf4c036a1e9ffb9b6804342f252a9de0268a
Christian Noriel <xtiannoriell@gmail.com>
2019-08-30 01:31:20 +0800

    Updates

---
+++ precache-manifest.54eba8a9d8871026a4f7e5f448a64ca4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50cc9fa831b8d953d62e5bb65179760d",
+    "url": "/reactjs-ecommerce/index.html"
+  },
+  {
+    "revision": "a7ba46be75a2b3935175",
+    "url": "/reactjs-ecommerce/static/css/main.792f057f.chunk.css"
+  },
+  {
+    "revision": "8444af34ba8813f4879f",
+    "url": "/reactjs-ecommerce/static/js/2.fdd637d5.chunk.js"
+  },
+  {
+    "revision": "a7ba46be75a2b3935175",
+    "url": "/reactjs-ecommerce/static/js/main.3fdd002b.chunk.js"
+  },
+  {
+    "revision": "b1943514f5d7a6d31903",
+    "url": "/reactjs-ecommerce/static/js/runtime~main.dca7b4fa.js"
+  }
+]);
